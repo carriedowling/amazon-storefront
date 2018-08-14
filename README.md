a8e48e416d146c7595c976488bc0602e18ab6536
carriedowling <37647285+carriedowling@users.noreply.github.com>
2018-08-13 20:38:18 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# amazon-storefront
+# node-mysql-amazonStoreFront
+
+## Introduction
+This is a node application that runs in the terminal and is a terminal storefront. Below are the instructions for use. 
+
+## Getting started
+### How the heck it works...
+
+First open the relative path you have cloned the repo to and run node (applicationname)
+** note: ** you need to make sure you have all the npm modules installed and have a working database on your machine with proper username and passward.
+
+### customerjs...
+For the customerjs the user will be promted what item ID they would like to access.
+![Screenshot of  Capture2 ](/assets/Capture2.png)
+
+Next the user will be prompted the quantity of the item they have selected. 
+![Screenshot of  Capture2 ](/assets/Capture2.png)
+
+Finally the terminal will display the quantity the user bought and the cost as well as the updated item.  It will also update the database accordingly.
+![Screenshot of  Capture2 ](/assets/Capture2.png)
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,70 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Valkyrie1!",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    displayData();
+});
+
+function displayData() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + " | " +
+                "Product name: " + res[i].product_name + " | " +
+                "Department: " + res[i].department_name + " | " +
+                "Price: " + res[i].price + " | " +
+                "Quantity in stock: " + res[i].stock_quantity)
+        };
+        console.log("\n*****************\n");
+        itemSelection();
+    })
+};
+
+function itemSelection() {
+    inquirer
+        .prompt([{
+            name: "userItemID",
+            type: "input",
+            message: "What item ID would you like to select?"
+        }, {
+            name: "userQuantity",
+            type: "input",
+            message: "How many of the item would you like?"
+        }])
+        .then(function (answer) {
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: answer.userItemID
+            }, function (err, res) {
+                if (err) throw err;
+                console.log(res[0].stock_quantity);
+                var updatedQuantity = res[0].stock_quantity - answer.userQuantity
+                if (answer.userQuantity < res[0].stock_quantity) {
+                    connection.query("UPDATE products SET stock_quantity = " + updatedQuantity + " WHERE item_id = " + answer.userItemID, function (err, data) {
+                        if (err) throw err;
+                        console.log("Your total is $" + res[0].price * answer.userQuantity);
+                        console.log("\n*****************\n");
+                        console.log("Item ID: " + res[0].item_id + " | " +
+                            "Product name: " + res[0].product_name + " | " +
+                            "Department: " + res[0].department_name + " | " +
+                            "Price: " + res[0].price + " | " +
+                            "Updated quantity in stock: " + updatedQuantity);
+                        console.log("\n*****************\n");
+                        console.log("Your total is $" + res[0].price * answer.userQuantity);
+                        connection.end();
+                    })
+                } else {
+                    console.log("Sorry, we do not have enough items to fulfill your request.");
+                    console.log("\n*****************\n");
+                }
+            })
+        })
+};
